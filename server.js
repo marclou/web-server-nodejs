e7a4a5d393afe776886fd68f8ea359b8292f0dac
marclou <marc.louvion@gmail.com>
2018-03-17 14:38:59 +0900

    set up for heroku deployment

--- server.js
+++ server.js
@@ -4,39 +4,37 @@ const fs = require('fs');
 
 const app = express();
 
-hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname+'/public'));
-
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
-     let reqDate = new Date();
-     let reqInfo = req.method + req.url;
+	let reqDate = new Date();
+	let reqInfo = req.method + req.url;
 
-     fs.appendFile('log.txt', reqDate + ' : ' + reqInfo + '\n', (err) => {
-          if (err) {
-               console.log(err);
-          }
-     });
-     next();
+	fs.appendFile('log.txt', reqDate + ' : ' + reqInfo + '\n', (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+	next();
 });
-
 app.use((req, res, next) => {
-     res.render('maintenance.hbs');
+	res.render('maintenance.hbs');
 });
 
 app.get('/', (req, res) => {
-     res.render('home.hbs', {
-          pageTitle: 'Home',
-          welcomeMessage: 'Welcome on the main page !',
-          date: new Date().getFullYear()
-     });
+	res.render('home.hbs', {
+		pageTitle: 'Home',
+		welcomeMessage: 'Welcome on the main page !',
+		date: new Date().getFullYear()
+	});
 });
 
 app.get('/about', (req, res) => {
-     res.render('about.hbs', {
-          pageTitle: 'About',
-          date: new Date().getFullYear()
-     });
+	res.render('about.hbs', {
+		pageTitle: 'About',
+		date: new Date().getFullYear()
+	});
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
