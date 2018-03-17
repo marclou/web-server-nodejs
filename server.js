10f388243b97c00f0904a7385e77c5d22b118c2b
marclou <marc.louvion@gmail.com>
2018-03-17 14:17:10 +0900

    adding dynamic views

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
+hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
 
-app.get('/', (req, res) => {
-     res.send('<h1>Root Dir</h1>');
+app.use((req, res, next) => {
+     let reqDate = new Date();
+     let reqInfo = req.method + req.url;
+
+     fs.appendFile('log.txt', reqDate + ' : ' + reqInfo + '\n', (err) => {
+          if (err) {
+               console.log(err);
+          }
+     });
+     next();
 });
+
+app.use((req, res, next) => {
+     res.render('maintenance.hbs');
+});
+
+app.get('/', (req, res) => {
+     res.render('home.hbs', {
+          pageTitle: 'Home',
+          welcomeMessage: 'Welcome on the main page !',
+          date: new Date().getFullYear()
+     });
+});
+
 app.get('/about', (req, res) => {
-     console.log(req);
-     res.render('about.hbs');
+     res.render('about.hbs', {
+          pageTitle: 'About',
+          date: new Date().getFullYear()
+     });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
