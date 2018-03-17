09981d9a4c23f226e098039cf9d16ff6aad155f2
marclou <marc.louvion@gmail.com>
2018-03-17 12:49:28 +0900

    based architecture settled down

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', (req, res) => {
      res.send('<h1>Root Dir</h1>');
+});
+app.get('/about', (req, res) => {
+     console.log(req);
+     res.render('about.hbs');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
